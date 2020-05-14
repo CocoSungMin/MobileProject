a21091a0f233ed733a3527c5c263fc5e87e88576
CocoSungMin <jm970326@naver.com>
2020-05-14 20:56:42 +0900

    일정 추가에 달력 띄우고 그걸로 날짜 지정함
    난 해냈다 !!!!! 하드코딩해라 이것들아

--- MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateSchedule.java
+++ MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateSchedule.java
@@ -3,6 +3,7 @@ package com.example.mobiletermproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,12 +21,14 @@ public class CreateSchedule extends AppCompatActivity {
     private CalendarDay endDate;
 
     private ArrayAdapter adapter;
-    private Spinner Yearstr;
-    private Spinner Monstr;
-    private Spinner Daystr;
-    private Spinner YearEnd;
-    private Spinner MonEnd;
-    private Spinner DayEnd;
+    private Spinner Hourstr;
+    private Spinner Mitstr;
+    private Spinner stramfm;
+    private Spinner HourEnd;
+    private Spinner MitEnd;
+    private Spinner endamfm;
+    private Button getDatestr;
+    private Button getDataend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +38,65 @@ public class CreateSchedule extends AppCompatActivity {
         schduleTitle = findViewById(R.id.schduleTitle);
         schduleContent = findViewById(R.id.schduleContent);
 
-        Yearstr = findViewById(R.id.YearSpinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.Year,android.R.layout.simple_spinner_dropdown_item);
-        Yearstr.setAdapter(adapter);
+        getDataend = findViewById(R.id.endDayButton);
+        getDatestr = findViewById(R.id.DayButton);
 
-        Monstr = findViewById(R.id.MonthSpinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.Month,android.R.layout.simple_spinner_dropdown_item);
-        Monstr.setAdapter(adapter);
+        stramfm = findViewById(R.id.stramfm);
+        endamfm = findViewById(R.id.endamfm);
+        adapter=ArrayAdapter.createFromResource(this,R.array.amfm,android.R.layout.simple_spinner_dropdown_item);
+        stramfm.setAdapter(adapter);
+        endamfm.setAdapter(adapter);
 
-        Daystr = findViewById(R.id.DaySpinner);
+        Hourstr = findViewById(R.id.Hour);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Hour,android.R.layout.simple_spinner_dropdown_item);
+        Hourstr.setAdapter(adapter);
 
-        YearEnd = findViewById(R.id.endYear);
-        adapter = ArrayAdapter.createFromResource(this,R.array.Year,android.R.layout.simple_spinner_dropdown_item);
-        YearEnd.setAdapter(adapter);
+        Mitstr = findViewById(R.id.Minute);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Minute,android.R.layout.simple_spinner_dropdown_item);
+        Mitstr.setAdapter(adapter);
 
-        MonEnd = findViewById(R.id.endMonth);
-        adapter = ArrayAdapter.createFromResource(this,R.array.Month,android.R.layout.simple_spinner_dropdown_item);
-        MonEnd.setAdapter(adapter);
+
+        HourEnd = findViewById(R.id.endHour);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Hour,android.R.layout.simple_spinner_dropdown_item);
+        HourEnd.setAdapter(adapter);
+
+        MitEnd = findViewById(R.id.endMinute);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Minute,android.R.layout.simple_spinner_dropdown_item);
+        MitEnd.setAdapter(adapter);
+
 
 
 
 
     }
-
-    public void onClickHandler(View view){
-        //DatePickerDialog dialog = new DatePickerDialog();
+    public void mOnPopupClick(View v) {
+        Intent intent = new Intent(this, DateSelection.class);
+        intent.putExtra("data", "Date select");
+        startActivityForResult(intent, 1);
     }
+    public void mOnPopupClick2(View v) {
+        Intent intent = new Intent(this, DateSelection.class);
+        intent.putExtra("data", "Date select");
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                getDatestr = findViewById(R.id.DayButton);
+                String temp = data.getStringExtra("result");
+                getDatestr.setText(temp);
+            }
+        } else if (requestCode == 2) {
+            if (resultCode == RESULT_OK) {
+                getDataend = findViewById(R.id.endDayButton);
+                String temp = data.getStringExtra("result");
+                getDataend.setText(temp);
+            }
+        }
+    }
+
+
 }
