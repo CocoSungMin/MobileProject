47fcec337f1d57d86a117817cd363e1c7c229b0c
CocoSungMin <jm970326@naver.com>
2020-05-15 01:14:08 +0900

    일정 추가되는거 구현했습니다.
    문제점은 아이디별 추가되는거 추가로 만드는 거랑 bottom sheet에서 조회되는거 만들어야 되서 조금 시간 걸릴꺼 같아요

--- MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateSchedule.java
+++ MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateSchedule.java
@@ -1,19 +1,41 @@
 package com.example.mobiletermproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opencensus.tags.Tag;
+import io.opencensus.tags.Tags;
+
 public class CreateSchedule extends AppCompatActivity {
+    private static final String TAG = "tag";
     private EditText schduleTitle;
     private EditText schduleContent;
 
@@ -29,6 +51,8 @@ public class CreateSchedule extends AppCompatActivity {
     private Spinner endamfm;
     private Button getDatestr;
     private Button getDataend;
+    private EditText title;
+    private EditText content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,6 +120,61 @@ public class CreateSchedule extends AppCompatActivity {
                 getDataend.setText(temp);
             }
         }
+    }
+
+    public void registerSchedule(View v){
+        final Intent intent = new Intent(this,Calendar_test.class);
+
+        title = findViewById(R.id.schduleTitle);
+        content = findViewById(R.id.schduleContent);
+        stramfm = findViewById(R.id.stramfm);
+        Hourstr = findViewById(R.id.Hour);
+        Mitstr = findViewById(R.id.Minute);
+        endamfm = findViewById(R.id.endamfm);
+        HourEnd = findViewById(R.id.endHour);
+        MitEnd = findViewById(R.id.endMinute);
+        String id = null;
+        FirebaseUser user1 = FirebaseAuth.getInstance().getCurrentUser();
+        if (user1 != null) {
+            String uid = user1.getUid();
+            id = uid;
+        }
+
+        if(id != null) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Map<String, Object> user = new HashMap<>();
+            user.put("id", id);
+            user.put("ScheduleName", title.getText().toString());
+            user.put("StartTime", stramfm.getSelectedItem().toString() + " " +
+                    Hourstr.getSelectedItem().toString() + " : " + Mitstr.getSelectedItem().toString());
+            user.put("Start Date", getDatestr.getText().toString());
+            user.put("EndTime", endamfm.getSelectedItem().toString() + " " +
+                    HourEnd.getSelectedItem().toString() + " : " +
+                    MitEnd.getSelectedItem().toString());
+            user.put("End Date", getDataend.getText().toString());
+            user.put("Content", content.getText().toString());
+
+            db.collection("users").
+                    add(user).
+                    addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID" + documentReference.getId());
+                            startActivity(intent);
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error adding document", e);
+                }
+            });
+        }
+        else{
+            Toast.makeText(this,"Fail",Toast.LENGTH_SHORT);
+        }
+
+
     }
 
 
