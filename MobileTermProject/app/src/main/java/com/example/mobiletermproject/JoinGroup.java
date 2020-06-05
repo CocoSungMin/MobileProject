7731e2641799dc58763dcfb4120030d0d4acd29b
jyoonlee <2jyoons@naver.com>
2020-06-06 00:58:20 +0900

    create group 부분 추가, 코드 정리
    안쓰는 코드 정리, group 부분 팝업 만들었어요

--- MobileTermProject/app/src/main/java/com/example/mobiletermproject/JoinGroup.java
+++ MobileTermProject/app/src/main/java/com/example/mobiletermproject/JoinGroup.java
@@ -1,6 +1,11 @@
 package com.example.mobiletermproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoinGroup extends AppCompatActivity {
@@ -9,8 +14,17 @@ public class JoinGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_group);
+
+
+        //스케쥴 추가 버튼
+        TextView btnAdd = findViewById(R.id.addBtn);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinGroup.this, CreateGroup.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    //그룹 조인 메소드
-
 }
