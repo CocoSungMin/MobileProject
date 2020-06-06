6cf1aad8f3b8d52f4e190f7c84a17356ec574095
jyoonlee <2jyoons@naver.com>
2020-06-06 22:55:31 +0900

    Join Group UI 완성, create Schedule 일부 수정
    Join Group UI 완성, create Schedule 일부 수정

--- MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateGroup.java
+++ MobileTermProject/app/src/main/java/com/example/mobiletermproject/CreateGroup.java
@@ -17,14 +17,10 @@ public class CreateGroup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        WindowManager.LayoutParams layoutParams= new WindowManager.LayoutParams();
-        layoutParams.flags= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        layoutParams.dimAmount= 0.5f;
-        getWindow().setAttributes(layoutParams);
         setContentView(R.layout.activity_create_group);
 
-        final EditText et_name = (EditText) findViewById(R.id.gr_name);
-        final EditText et_url = (EditText) findViewById(R.id.gr_number);
+        final EditText gr_name = (EditText) findViewById(R.id.gr_name);
+        final EditText gr_number = (EditText) findViewById(R.id.gr_number);
 
         //확인버튼 이벤트
         Button button_ok = (Button) findViewById(R.id.bt_ok);
@@ -32,11 +28,11 @@ public class CreateGroup extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //데이터 전달하고 액티비티 닫기
-                String name = et_name.getText().toString();
-                String url = et_url.getText().toString();
+                String name = gr_name.getText().toString();
+                String number = gr_number.getText().toString();
                 Intent intent = new Intent();
-                intent.putExtra("name", name);
-                intent.putExtra("url", url);
+                intent.putExtra("그룹이름 : ", name);
+                intent.putExtra("인원 제한 : ", number);
                 setResult(RESULT_OK, intent);
                 finish();
             }
