4f39b7364331fa24878f4785a205052da2eccbfc
CocoSungMin <jm970326@naver.com>
2020-06-17 18:34:08 +0900

    스케쥴 전체 삭제 구현
    그렇습니다.

--- MobileTermProject/app/src/main/java/com/LEGENO/SaveTheTime/Setting.java
+++ MobileTermProject/app/src/main/java/com/LEGENO/SaveTheTime/Setting.java
@@ -6,13 +6,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Setting extends AppCompatActivity {
 
     String userName;
     String userEmail;
+    String uid = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,10 +74,52 @@ public class Setting extends AppCompatActivity {
         deleteSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                deleteAll();
                 Intent intent = new Intent(Setting.this, ConfirmDeleteAllSchedule.class);
                 startActivity(intent);
             }
         });
+    }
+    // 일정 전체 삭
+    public void deleteAll(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user  = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user!= null){
+            uid = user.getUid();
+        }
+
+        db.collection(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                deleteDocument(document.getId());
+                            }
+                        } else {
+                        }
+                    }
+                });
+
+    }
+
+    //deleteAll()에서 찾은 document id 로 다 지
+    public void deleteDocument(String docName){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(uid).document(docName)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
 
 
